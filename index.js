8f6631ae4993186fe46bad27ecbfc6547bd3eb8a
yoosangwook <yoo32767@gmail.com>
2025-03-04 18:21:52 +0900

    fix: 이미지 저장 경로 수정

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ app.post("/image/canvas", upload.single("file"), async (req, res) => {
   console.log("left: ", left);
   console.log("top: ", top);
 
-  const FILE_PATH = `public/Drawing/${objectNo}`;
+  const FILE_PATH = `public/Drawing`;
   try {
     await fs.readdir(FILE_PATH);
   } catch {
@@ -186,12 +186,11 @@ app.post("/image/canvas", upload.single("file"), async (req, res) => {
     __dirname,
     "public",
     "uploads",
-    objectNo,
     req.file.filename
   );
   console.log("🚀 ~ app.post ~ uploadedFilePath:", uploadedFilePath);
-  console.log(`public/uploads/${objectNo}/${req.file.filename}`);
-  const imagePath = `public/uploads/${objectNo}/${req.file.filename}`;
+  console.log(`public/uploads/${req.file.filename}`);
+  const imagePath = `public/uploads/${req.file.filename}`;
 
   try {
     const metadata = await sharp(imagePath).metadata();
